f8c18aa363c2789760cbcc7e2259382ccea41231
zhoujie15 <zhoujie19940804@126.com>
2015-03-25 14:30:14 +0800

    Create 04-02-addr.py

---
+++ all/04-02-addr.py
@@ -0,0 +1,72 @@
+# Input: memory
+#   The memory description given by teacher
+#
+# Type the hex number of the virtual address on screen
+#
+# Output: answer
+#   The answer for each input in a fixed format
+#
+# Author: zhoujie15
+#
+# Time: 2015-03-18
+
+fin = open('memory.py', 'rb')
+fout = open('answer', 'wb')
+memory = []
+disk = []
+flag = False
+for line in fin:
+    if line.strip('\r\n') == '':
+        flag = True
+        continue
+    k = line.find(':')
+    line = line[k+2:]
+    arr = line.strip('\r\n').split(' ')
+    for item in arr:
+        if item != '':
+            if not flag:
+                memory.append(int(item, 16))
+            else:
+                disk.append(int(item, 16))
+print len(disk)
+print len(memory)
+pdbr = 0xd80
+
+while True:
+    num = raw_input()
+    if num == '':
+        break
+    va = int(num, 16)
+    fout.write('Virtual Address ' + num + ':\n')
+
+    of1 = (va & 0x7c00) >> 10
+    of2 = (va & 0x3e0) >> 5
+    of3 = va & 0x1f
+
+    fout.write('  --> pde index:' + str(hex(of1)))
+
+    pde = memory[pdbr + of1]
+    valid = (pde & 0x80) >> 7
+    pde = pde & 0x7f
+    fout.write('  pde contents:(valid ' + str(valid) + ', pfn ' + str(hex(pde)) +')\n')
+    if valid == 0:
+        fout.write('      --> Fault (page directory entry not valid)\n')
+        continue
+
+    ad = (pde << 5) + of2
+
+
+
+    pte = memory[ad]
+    valid = (pte & 0x80) >> 7
+    pte = pte & 0x7f
+    pa = (pte << 5) + of3
+
+    fout.write('    --> pte index:' + str(hex(of2)) + '  pte content:(valid ' + str(valid) + ', pfn ' + str(hex(pte)) + ')\n')
+    if valid == 0:
+        fout.write('      --> To Disk Sector Address ' + str(hex(pa)) + ' --> Value: ' + str(hex(disk[pa])) + '\n')
+        continue
+
+    fout.write('      --> Translate to Physical Address ' + str(hex(pa)) + ' --> Value: ' + str(hex(memory[pa])) + '\n')
+
+
